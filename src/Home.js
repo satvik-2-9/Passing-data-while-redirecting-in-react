f9148d42438414f535eb46e4290f3bb8107539e0
unknown <satvikbatra@gmail.com>
2021-09-09 12:47:20 +0530

    using props history

--- src/Home.js
+++ src/Home.js
@@ -2,28 +2,29 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 
-function Home() {
-  /* function g() {
-    window.location.href = "./pp";
-      return (<Redirect
-        to={{
-          path: "/pp",
-          state: { verifier_id: 2 }
-        }}
-      />
-    )
+function Home(props) {
+ 
+  
+  function g(event) {
+    event.preventDefault();
+    props.history.push({
+      pathname: '/next',
+      state:{id:3}
+    });
   };
-     */
 
     return (
         <div>
         <h1>hi</h1>
-        <Link to={{
-          pathname: "/next",
-          state:{id:5}
-          }}>
+        
+        <button onClick={g}> click me! </button>
+        
+        {/* <Link to={{
+            pathname: "/next",
+            state: { id: 5 }
+        }}>
            hit it !
-        </Link>
+        </Link> */}
         </div>
     )
 }
